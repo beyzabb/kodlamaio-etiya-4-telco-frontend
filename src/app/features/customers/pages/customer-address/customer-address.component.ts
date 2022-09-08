bbc7ccbdb9b9914624a84471877cb1f2c825e053
Beyza <bbasaga06@gmail.com>
2022-09-09 02:28:21 +0300

    Add delete customer in customer-info

--- src/app/features/customers/pages/customer-address/customer-address.component.ts
+++ src/app/features/customers/pages/customer-address/customer-address.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { Address } from '../../models/address';
+import { Customer } from '../../models/customer';
 import { CustomersService } from '../../services/customer/customers.service';
 
 @Component({
@@ -10,15 +12,26 @@ import { CustomersService } from '../../services/customer/customers.service';
 export class CustomerAddressComponent implements OnInit {
   selectedCustomerId!: number;
   customerAddress: Address[] = [];
+  addressToDelete!: Address;
+  customer!: Customer;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private customerService: CustomersService,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
     this.getCustomerById();
+    this.messageService.clearObserver.subscribe((data) => {
+      if (data == 'r') {
+        this.messageService.clear();
+      } else if (data == 'c') {
+        this.messageService.clear();
+        this.remove();
+      }
+    });
   }
 
   getCustomerById() {
@@ -31,6 +44,8 @@ export class CustomerAddressComponent implements OnInit {
       this.customerService
         .getCustomerById(this.selectedCustomerId)
         .subscribe((data) => {
+          this.customer = data;
+          this.customerAddress = [];
           data.addresses?.forEach((adress) => {
             this.customerAddress.push(adress);
           });
@@ -48,5 +63,31 @@ export class CustomerAddressComponent implements OnInit {
     this.router.navigateByUrl(
       `/dashboard/customers/${this.selectedCustomerId}/address/update/${addressId}`
     );
+  }
+
+  removePopup(address: Address) {
+    if (this.customer.addresses && this.customer.addresses?.length <= 1) {
+      this.messageService.add({
+        detail:
+          'The address that you want to delete is a default address. Please, change default address then try again.',
+        key: 'etiya-warn',
+      });
+      return;
+    }
+    this.addressToDelete = address;
+    this.messageService.add({
+      key: 'c',
+      sticky: true,
+      severity: 'warn',
+      detail: 'Your changes could not be saved. Are you sure?',
+    });
+  }
+
+  remove() {
+    this.customerService
+      .removeAddress(this.addressToDelete, this.customer)
+      .subscribe((data) => {
+        this.getCustomerById();
+      });
   }
 }
