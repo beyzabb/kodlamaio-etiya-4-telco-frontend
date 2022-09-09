ab2e8ee5d7a3c1a3aa5c6983821bdcf41d6c61a9
Beyza <bbasaga06@gmail.com>
2022-09-10 02:59:10 +0300

    Create product-detail-pop-up

--- src/app/shared/components/table-accordion/table-accordion.component.ts
+++ src/app/shared/components/table-accordion/table-accordion.component.ts
@@ -1,6 +1,8 @@
 import { Product } from './../../../features/customers/models/product';
 import { Component, Input, OnInit } from '@angular/core';
 import { BillingAccount } from 'src/app/features/customers/models/billingAccount';
+import { Offer } from 'src/app/features/offers/models/offer';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-table-accordion',
@@ -11,7 +13,31 @@ export class TableAccordionComponent implements OnInit {
   @Input() billingAccount!: BillingAccount;
   @Input() customerId!: number;
 
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   ngOnInit(): void {}
+
+  productDetail(billingAccount: BillingAccount, offer: Offer) {
+    if (offer.type.typeName == 'campaign') {
+      let prodCampaignOfferId = offer.id.toString();
+      let prodCampaignOfferName = offer.name;
+      let prodCampaignId = offer.type.id.toString();
+      this.messageService.add({
+        key: 'product-detail',
+        sticky: true,
+        detail: prodCampaignOfferId + prodCampaignOfferName + prodCampaignId,
+      });
+      let custCampaignBillAddress = billingAccount.addresses;
+    } else if (offer.type.typeName == 'catalog') {
+      let prodCatalogOfferId = offer.id.toString();
+      let prodCatalogOfferName = offer.name;
+      let custCatalogBillAddress = billingAccount.addresses;
+      this.messageService.add({
+        key: 'product-detail',
+        sticky: true,
+        severity: 'warn',
+        detail: prodCatalogOfferId + prodCatalogOfferName,
+      });
+    }
+  }
 }
