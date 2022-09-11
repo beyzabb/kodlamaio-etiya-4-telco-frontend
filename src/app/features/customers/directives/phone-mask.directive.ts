d8f4276ccf38827045e07c623b2d9efc404ed16b
Beyza <bbasaga06@gmail.com>
2022-09-11 16:23:39 +0300

    Update phoneMask

--- src/app/features/customers/directives/phone-mask.directive.ts
+++ src/app/features/customers/directives/phone-mask.directive.ts
@@ -27,10 +27,10 @@ export class PhoneMaskDirective {
     } else if (newVal.length <= 3) {
       newVal = newVal.replace(/^(\d{0,3})/, '($1)');
     } else if (newVal.length <= 10) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1)$2$3');
     } else {
       newVal = newVal.substring(0, 10);
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1)$2$3');
     }
     this.ngControl.valueAccessor?.writeValue(newVal);
   }
